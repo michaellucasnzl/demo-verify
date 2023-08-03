153d8e46e0f804f61240c6311997f877cefac302
Michael Lucas <michael@samwise.nz>
2023-08-03 21:55:01 +1200

    added person object test

--- app.Test/Tests.cs
+++ app.Test/Tests.cs
@@ -1,10 +1,13 @@
+using System.Drawing;
+using FluentAssertions;
+
 namespace app.Test;
 
 [UsesVerify]
 public class Tests
 {
     [Fact]
-    public async Task PersonTest()
+    public void Person_AssertTest()
     {
         var person = new Person()
         {
@@ -18,6 +21,36 @@ public class Tests
             }
         };
 
+        person.Name.Should().Be("Joe Bloggs");
+        person.Address.AddressLine1.Should().Be("1 Test Ave");
+        person.Address.City.Should().Be("Test City");
+        person.Address.Country.Should().Be("New Zealand");
+        person.Address.PostCode.Should().Be("1234");
+        person.Address.Suburb.Should().BeNull();
+        person.Address.AddressLine2.Should().BeNull();        
+    }
+
+    [Fact]
+    public async Task Person_VerifyTest()
+    {
+        var person = new Person()
+        {
+            Name = "Joe Bloggs",
+            Address = new Address()
+            {
+                AddressLine1 = "1 Test Ave",
+                City = "Test City",
+                Country = "New Zealand",
+                PostCode = "1234"
+            }
+        };
+        
         await Verify(person);
     }
+
+   
+
+
+
+
 }
